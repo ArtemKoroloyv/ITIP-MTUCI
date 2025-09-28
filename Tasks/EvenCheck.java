aceef34c1b09d48998c1ed29bfb59fcf61bb5fe0
ArtemKoroloyv <artemkoroloyv25@gmail.com>
2025-09-28 23:22:18 +0300

    Create EvenCheck.java

---
+++ Tasks/EvenCheck.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EvenCheck {
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isEven(n));
+    }
+}
