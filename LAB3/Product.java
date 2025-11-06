284a737cd393820270bc798f95973c7be9f6329c
ArtemKoroloyv <artemkoroloyv25@gmail.com>
2025-11-07 02:26:22 +0300

    Add Product class with attributes and methods

---
+++ LAB3/Product.java
@@ -0,0 +1,37 @@
+class Product {
+    private final String sku;   // артикул
+    private String name;        // наименование
+    private String description; // описание
+    private double price;       // цена
+    private int quantity;       // количество на складе
+
+    public Product(String sku, String name, String description, double price, int quantity) {
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getSku() { return sku; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setPrice(double price) { this.price = price; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    @Override
+    public String toString() {
+        return "Product [" +
+                "sku='" + sku + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ']';
+    }
+}
